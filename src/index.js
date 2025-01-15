44e7978846157286150e93190c92ff3e3af7bcfe
S4lvatoore <david.sladkov@internet.ru>
2025-01-15 17:12:47 +0400

    setup routing with Root component

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import store from "./redux/store";
+import Root from "./routes/root";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+    },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <RouterProvider router={router} />
+        </Provider>
+    </React.StrictMode>
+);
